e73557cc2f8c8c272067641ddba427440b0c197a
tripathinilakshi20 <tripathinilakshi20@gmail.com>
2026-06-19 15:51:06 +0530

    day 01 completed nilakshi tripathi

--- assignments/day-01/submissions/batch-a/nilakshi-tripathi/Program01_print_name_age_collegename.py
+++
@@ -1,9 +0,0 @@
-# 1. Print your name, age, and college name.
-
-student_name=input("Enter the name of student:")
-student_age=int(input("Enter the age of the student:"))
-college_name=input("Enter the college name:")
-
-print("Student name:",student_name)
-print("Student age:",student_age)
-print("College name:",college_name)
